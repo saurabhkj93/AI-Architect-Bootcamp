4e6579c16f7199430c44e3100a43882ecac6180a
Saurabh Kashyap <saurabhkj93@gmail.com>
2026-06-29 20:25:56 +0530

    feat: add first Python project and greeting program

---
+++ projects/day01/hello_builder.py
@@ -0,0 +1,10 @@
+def main():
+    hello("Saurabh")
+    print("Today we build systems, not scripts.")
+
+def hello(name):
+    print(f"Welcome {name}.")
+    
+
+if __name__ == "__main__":
+    main()
