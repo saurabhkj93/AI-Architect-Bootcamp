5589afef5d2f2ba57415ae15dad46819df751c9e
Saurabh Kashyap <saurabhkj93@gmail.com>
2026-07-01 20:56:35 +0530

    feat: introduce variables and demonstrate state with basic program

---
+++ projects/day02/variables.py
@@ -0,0 +1,19 @@
+project_name = "Project Builder"
+project_description = "Documenting the journey of becoming an engineer who builds AI systems that matter."
+project_version = "0.8"
+author_name = "Saurabh Kashyap"
+author_email = "saurabhkj93@gmail.com"
+execution_instructions = "Run the main() function."
+
+
+def main():
+    print(f"Project Name        : {project_name}")
+    print(f"Version             : {project_version}")
+    print(f"Author              : {author_name}")
+    print(f"Author Email        : {author_email}")
+    print(f"Description         : {project_description}")
+    print(f"Instructions        : {execution_instructions}")
+
+
+if __name__ == "__main__":
+    main()
